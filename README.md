815f4d40b529c2a895a51da0d679d28977d8762b
Greg ORIOL <greg@gregoriol.net>
2022-11-11 16:02:09 +0100

    Imported first version

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # check_systemd
 Nagios check script to count systemd units by state and alert
+
+## Requires
+Nothing special
+
+## Setup
+Download check_systemd zip file or clone it into your target server, then
+```$ php composer.phar install```
+```$ php composer.phar dump-autoload -o```
+
+and call it remotely with `check_by_ssh`
+
+## Usage
+Nagios configuration:
+```
+define command {
+        command_name    check_by_ssh_check_systemd
+        command_line    $USER1$/check_by_ssh -H $HOSTADDRESS$ -p 143 -C "php /home/nagios/check_systemd/check_systemd.php"
+}
+
+define service {
+        use                             generic-service
+        host_name                       myhost
+        service_description             my-service
+        check_command                   check_systemd
+}
+```
+
+## Checks performed
+Currently, checks if there are failed units and reports a critical status when there is at least 1.
---
+++ check_systemd.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once(__DIR__.'/vendor/autoload.php');
+
+require_once(__DIR__.'/inc/nagios.php');
+
+// Command line parser
+$parser = new Console_CommandLine(array(
+	'description'	=> 'Check systemd failed',
+	'version'		=> '0.0.1',
+	'force_posix'	=> true
+));
+
+// $parser->addOption('state', array(
+// 	// 'short_name'	=> '-u',
+// 	'long_name'		=> '--state',
+// 	'description'	=> 'State of the service',
+// 	'action'		=> 'StoreString'
+// ));
+
+try {
+	$result = $parser->parse();
+
+	// $state = $result->options['state'];
+	// if (empty($state)) {
+		$state = 'failed';
+	// }
+
+	exec('systemctl list-units --state='.$state.' --full --plain --no-legend --no-pager', $output, $return);
+	if ($return !== 0) {
+		echo 'Couldn\'t retrieve output from systemctl: returned '.$return."\n";
+		exit(NAGIOS_UNKNOWN);
+	}
+
+	if (empty($output)) {
+		echo 'No units in '.$state.' state'."\n";
+		exit(NAGIOS_OK);
+	}
+
+	$units = [];
+	foreach ($output as $row) {
+		$r = preg_match('/^([^.]+\.[^ ]+) +([^ ]+) +([^ ]+) +([^ ]+) +(.+)$/', $row, $matches);
+		if (!$r || count($matches) !== 6) {
+			echo 'Couldn\'t parse output'."\n";
+			exit(NAGIOS_UNKNOWN);
+		}
+
+		$unit = $matches[1];
+		$load = $matches[2];
+		$active = $matches[3];
+		$sub = $matches[4];
+		$description = $matches[5];
+
+		$units[] = $unit;
+	}
+
+	$c = count($units);
+	if ($c === 1) {
+		echo ''.count($units).' unit in '.$state.' state: '.join(', ', $units)."\n";
+	} else {
+		echo ''.count($units).' units in '.$state.' state: '.join(', ', $units)."\n";
+	}
+	exit(NAGIOS_CRITICAL);
+
+} catch (Exception $exc) {
+	$parser->displayError($exc->getMessage());
+	exit(NAGIOS_UNKNOWN);
+}
